ef2412d5a96f1f528592eec3f422f9c5c83ec88d
Gustavo Barreto <gbarreto.alves2@gmail.com>
2021-04-30 18:04:42 -0300

    Demo Version
    Nesta versão pode-se entender melhor o funcionamento do app.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,77 @@
 //essas constantes são para facilitar o uso do querySelector(all)
 const c = (el)=> document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
-
+//Mapeia a lista de verbo de acordo com o verboJson
 verboJson.map((item, index)=>{
-    let verboItem = c('.models .verbo-area').cloneNode(true);
-    
-    c('#name-verbo').innerHTML = item.name;
-    verboItem.querySelector('a').addEventListener('click', (e)=>{ //Adiciona um evento
-        e.preventDefault(); //Retira as ações padrões a tag <a>
-        
+    let verboItem = c('.models .verbo-area').cloneNode(true); //clona a div modelo
+    //Lista os verbos de acordo com o verboJson
+    verboItem.setAttribute('id', index);
+    verboItem.querySelector('#name-verbo').innerHTML = item.name;
+    verboItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        let key = e.target.closest('.verbo-area').getAttribute('id');
+
+        //Adciona informação na tela de acordo com o verbo clicado
         c('.verb-content').innerHTML = item.name;
-        console.log(verboJson[item.id-1]); //Esse -1 é para controlar o id a seu respectivo conteudo
-        
-        
-        
-        //Quando o user clica no verbo, ativa a tela 4
+        c('.traducao-text').innerHTML = item.traducao;
+        //Troca da tela 3 para a tela 4 quando clica no verbo
         c('.screen3').style.display = 'none';
         c('.screen4').style.display = 'flex';
-    });
+    })
     //esse comando serve para somar a div que está sendo preechida
     c('.screen3').append( verboItem );
 });
 
+//-----------------NEXT e BACK------------
+
+//Tela 4 volta para 3
+function backScreen(){
+    c('.screen4').style.display = 'none';
+    c('.screen3').style.display = 'flex';
+}
+//Tela 3 volta para 2
+function backScreen2(){
+    c('.screen3').style.display = 'none';
+    c('.logo-area').style.display = 'none';
+    c('.screen2').style.display = 'flex';
+}
+//Tela 2 volta para 1
+function backScreen3(){
+    c('.screen2').style.display = 'none';
+    c('.screen1').style.display = 'flex';
+}
+//Tela 1 avança para 2
+function nextScreen(){
+    c('.screen1').style.display = 'none';
+    c('.screen2').style.display = 'flex';
+}
+//Tela 2 avança para 3
+function nextScreen2(){
+    c('.screen2').style.display = 'none';
+    c('.logo-area').style.display = 'flex';
+    c('.screen3').style.display = 'flex';
+}
+//Tela 4 avança para 5
+function nextScreen3(){
+    c('.screen4').style.display = 'none';
+    c('.screen5').style.display = 'flex';
+}
+//Tela 4 avança para 5
+function backScreen4(){
+    c('.screen5').style.display = 'none';
+    c('.screen4').style.display = 'flex';
+}
+
+
+//função para marcar o verbo como feito (NÃO FUNCIONA)
+/*function checked(){
+    if(document.querySelector('.border-verbo').closest('.check-icon').classList.contains('border-verbo')){
+        console.log("tem");
+        c('.border-verbo').classList.remove('border-verbo');
+        c('.border-verbo').classList.add('verbochecked');
+    
+    }else{
+        c('.border-verbo').classList.remove('verbochecked');
+        c('.border-verbo').classList.add('border-verbo');           
+    }}*/
--- js/verbos.js
+++ js/verbos.js
@@ -1,5 +1,12 @@
 let verboJson = [
-    {id:1, name:'teste 1', traducao:'Perguntar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
-    {id:2, name:'teste 2', traducao:'Perguntar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
-    {id:3, name:'teste 3', traducao:'Perguntar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'}
+    {id:0, name:'To add', traducao:'Adicionar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:1, name:'To ask', traducao:'Perguntar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:2, name:'To be', traducao:'Ser/Estar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:3, name:'To allow', traducao:'Permitir/Autorizar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:4, name:'To appear', traducao:'Aparecer', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:5, name:'To become', traducao:'Tornar-se', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:6, name:'To believe', traducao:'Acreditar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:7, name:'To build', traducao:'Construir', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:8, name:'To call', traducao:'Telefonar/Chamar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'},
+    {id:9, name:'To change', traducao:'Mudar/Alterar', audio:'audio.mp3',  exemplo:'I want to ask you a question, if you don’t mind.', tradExemplo: 'Eu quero te fazer uma pergunta, se você não se importa.'}
 ];
